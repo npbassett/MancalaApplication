ae7002ad11e5158de1b48337b63e5abc55568bb9
npbassett <bassett.neil@gmail.com>
2022-10-14 17:05:56 -0600

    Created ViewModel.

---
+++ app/src/main/java/com/example/mancalaapplication/MancalaViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.mancalaapplication
+
+import androidx.lifecycle.ViewModel
+
+class MancalaViewModel : ViewModel() {
+    //create model which contains data about the board for the UI
+    private val mancala_model = MancalaModel()
+}
