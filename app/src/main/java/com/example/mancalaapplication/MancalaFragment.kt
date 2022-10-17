69ae7b30f0277b86a78d84ea4877203f53181d32
npbassett <bassett.neil@gmail.com>
2022-10-17 16:10:52 -0600

    Moved game logic functions to viewModel.

--- app/src/main/java/com/example/mancalaapplication/MancalaFragment.kt
+++ app/src/main/java/com/example/mancalaapplication/MancalaFragment.kt
@@ -1,17 +1,21 @@
 package com.example.mancalaapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.mancalaapplication.databinding.MancalaFragmentBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 
 class MancalaFragment : Fragment(R.layout.mancala_fragment) {
 
-    private var mancalaModel = MancalaModel()
+    private val viewModel: MancalaViewModel by viewModels()
+
+    //private var mancalaModel = MancalaModel()
 
     private lateinit var binding: MancalaFragmentBinding
 
@@ -20,7 +24,32 @@ class MancalaFragment : Fragment(R.layout.mancala_fragment) {
     ): View {
         // Inflate the layout XML file and return a binding object instance
         binding = MancalaFragmentBinding.inflate(inflater, container, false)
+        Log.d("MancalaFragment", "MancalaFragment created/re-created!")
         return binding.root
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("MancalaFragment", "MancalaFragment destroyed!")
+    }
+
+    private fun updateDisplay() {
+        binding.tvPlayersTurn.text = if (viewModel.player1Turn) "Player 1's turn"
+        else "Player 2's turn"
+        binding.tvPocket0.text = getString(R.string.stones, viewModel.pocketStones[0])
+        binding.tvPocket1.text = getString(R.string.stones, viewModel.pocketStones[1])
+        binding.tvPocket2.text = getString(R.string.stones, viewModel.pocketStones[2])
+        binding.tvPocket3.text = getString(R.string.stones, viewModel.pocketStones[3])
+        binding.tvPocket4.text = getString(R.string.stones, viewModel.pocketStones[4])
+        binding.tvPocket5.text = getString(R.string.stones, viewModel.pocketStones[5])
+        binding.tvPocket6.text = getString(R.string.stones, viewModel.pocketStones[6])
+        binding.tvPocket7.text = getString(R.string.stones, viewModel.pocketStones[7])
+        binding.tvPocket8.text = getString(R.string.stones, viewModel.pocketStones[8])
+        binding.tvPocket9.text = getString(R.string.stones, viewModel.pocketStones[9])
+        binding.tvPocket10.text = getString(R.string.stones, viewModel.pocketStones[10])
+        binding.tvPocket11.text = getString(R.string.stones, viewModel.pocketStones[11])
+        binding.tvPocket12.text = getString(R.string.stones, viewModel.pocketStones[12])
+        binding.tvPocket13.text = getString(R.string.stones, viewModel.pocketStones[13])
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,158 +57,65 @@ class MancalaFragment : Fragment(R.layout.mancala_fragment) {
         // Update UI to reflect initial number of stones in each pocket.
         updateDisplay()
         // set up click listener on each button
-        binding.btnPocket0.setOnClickListener { moveStones(0) }
-        binding.btnPocket1.setOnClickListener { moveStones(1) }
-        binding.btnPocket2.setOnClickListener { moveStones(2) }
-        binding.btnPocket3.setOnClickListener { moveStones(3) }
-        binding.btnPocket4.setOnClickListener { moveStones(4) }
-        binding.btnPocket5.setOnClickListener { moveStones(5) }
-        binding.btnPocket7.setOnClickListener { moveStones(7) }
-        binding.btnPocket8.setOnClickListener { moveStones(8) }
-        binding.btnPocket9.setOnClickListener { moveStones(9) }
-        binding.btnPocket10.setOnClickListener { moveStones(10) }
-        binding.btnPocket11.setOnClickListener { moveStones(11) }
-        binding.btnPocket12.setOnClickListener { moveStones(12) }
+        binding.btnPocket0.setOnClickListener { onButtonClick(0) }
+        binding.btnPocket1.setOnClickListener { onButtonClick(1) }
+        binding.btnPocket2.setOnClickListener { onButtonClick(2) }
+        binding.btnPocket3.setOnClickListener { onButtonClick(3) }
+        binding.btnPocket4.setOnClickListener { onButtonClick(4) }
+        binding.btnPocket5.setOnClickListener { onButtonClick(5) }
+        binding.btnPocket7.setOnClickListener { onButtonClick(7) }
+        binding.btnPocket8.setOnClickListener { onButtonClick(8) }
+        binding.btnPocket9.setOnClickListener { onButtonClick(9) }
+        binding.btnPocket10.setOnClickListener { onButtonClick(10) }
+        binding.btnPocket11.setOnClickListener { onButtonClick(11) }
+        binding.btnPocket12.setOnClickListener { onButtonClick(12) }
     }
 
-    private fun updateDisplay() {
-        binding.tvPlayersTurn.text = if (mancalaModel.player1Turn) "Player 1's turn"
-            else "Player 2's turn"
-        binding.tvPocket0.text = getString(R.string.stones, mancalaModel.pocketStones[0])
-        binding.tvPocket1.text = getString(R.string.stones, mancalaModel.pocketStones[1])
-        binding.tvPocket2.text = getString(R.string.stones, mancalaModel.pocketStones[2])
-        binding.tvPocket3.text = getString(R.string.stones, mancalaModel.pocketStones[3])
-        binding.tvPocket4.text = getString(R.string.stones, mancalaModel.pocketStones[4])
-        binding.tvPocket5.text = getString(R.string.stones, mancalaModel.pocketStones[5])
-        binding.tvPocket6.text = getString(R.string.stones, mancalaModel.pocketStones[6])
-        binding.tvPocket7.text = getString(R.string.stones, mancalaModel.pocketStones[7])
-        binding.tvPocket8.text = getString(R.string.stones, mancalaModel.pocketStones[8])
-        binding.tvPocket9.text = getString(R.string.stones, mancalaModel.pocketStones[9])
-        binding.tvPocket10.text = getString(R.string.stones, mancalaModel.pocketStones[10])
-        binding.tvPocket11.text = getString(R.string.stones, mancalaModel.pocketStones[11])
-        binding.tvPocket12.text = getString(R.string.stones, mancalaModel.pocketStones[12])
-        binding.tvPocket13.text = getString(R.string.stones, mancalaModel.pocketStones[13])
+    private fun wrongSideSnackbar() {
+        activity?.let { it ->
+            Snackbar.make(
+                it.findViewById(R.id.coordinatorLayout),
+                R.string.pocket_wrong_side_snackbar,
+                Snackbar.LENGTH_SHORT
+            ).setAction(R.string.dismiss) {}
+        }?.show()
+        return
     }
 
-    private fun checkBottomEmpty(): Boolean {
-        for (i in 0..5) {
-            if (mancalaModel.pocketStones[i] != 0) {
-                return false
-            }
+    private fun emptyPocketSnackbar() {
+        activity?.let { it ->
+            Snackbar.make(
+                it.findViewById(R.id.coordinatorLayout),
+                R.string.pocket_empty_snackbar,
+                Snackbar.LENGTH_SHORT
+            ).setAction(R.string.dismiss) {}
+        }?.show()
+        return
+    }
+
+    private fun onButtonClick(pocket: Int) {
+        if (viewModel.pocketWrongSide(pocket)) {
+            wrongSideSnackbar()
+        } else if (viewModel.pocketEmpty(pocket)) {
+            emptyPocketSnackbar()
+        } else {
+            viewModel.moveStones(pocket)
+            updateDisplay()
+            if (viewModel.gameOver) showWinnerDialog()
         }
-        return true
-    }
-
-    private fun checkTopEmpty(): Boolean {
-        for (i in 7..12) {
-            if (mancalaModel.pocketStones[i] != 0) {
-                return false
-            }
-        }
-        return true
-    }
-
-    private fun checkPlayer1Winner(): Boolean {
-        return mancalaModel.pocketStones[6] > mancalaModel.pocketStones[13]
-    }
-
-    private fun restartGame() {
-        mancalaModel = MancalaModel()
-        updateDisplay()
     }
 
     private fun showWinnerDialog() {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.congratulations))
-            .setMessage(getString(R.string.winner, if (checkPlayer1Winner()) "Player 1" else "Player2"))
+            .setMessage(getString(R.string.winner, if (viewModel.checkPlayer1Winner()) "Player 1" else "Player2"))
             .setCancelable(false)
-            .setPositiveButton(getString(R.string.play_again)) { _, _ -> restartGame() }
+            .setPositiveButton(getString(R.string.play_again)) { _, _ ->
+                run {
+                    viewModel.restartGame()
+                    updateDisplay()
+                }
+            }
             .show()
-    }
-
-    private fun moveStones(pocket: Int) {
-        val playersStore = if (mancalaModel.player1Turn) 6 else 13
-        val otherPlayersStore = if (mancalaModel.player1Turn) 13 else 6
-        val playersPockets = if (mancalaModel.player1Turn) {
-            mutableListOf(0, 1, 2, 3, 4, 5)
-        } else {
-            mutableListOf(7, 8, 9, 10, 11, 12)
-        }
-        val otherplayersPockets = if (mancalaModel.player1Turn) {
-            mutableListOf(7, 8, 9, 10, 11, 12)
-        } else {
-            mutableListOf(0, 1, 2, 3, 4, 5)
-        }
-        // if selected pocket is on the wrong side of the board, prompt user ot pick another pocket
-        if (pocket in otherplayersPockets) {
-            activity?.let { it ->
-                Snackbar.make(
-                    it.findViewById(R.id.coordinatorLayout),
-                    R.string.wrong_side_snackbar,
-                    Snackbar.LENGTH_SHORT
-                ).setAction(R.string.dismiss) {}
-            }?.show()
-            return
-        }
-        // if selected pocket is empty, prompt user to pick another pocket
-        if (mancalaModel.pocketStones[pocket] == 0) {
-            activity?.let { it ->
-                Snackbar.make(
-                    it.findViewById(R.id.coordinatorLayout),
-                    R.string.pocket_empty_snackbar,
-                    Snackbar.LENGTH_SHORT
-                ).setAction(R.string.dismiss) {}
-            }?.show()
-            return
-        }
-        var numStonesToMove: Int = mancalaModel.pocketStones[pocket]
-        mancalaModel.pocketStones[pocket] = 0
-        var currentPocket = pocket + 1
-        // distribute stones into pockets
-        while (numStonesToMove > 0) {
-            if (currentPocket == otherPlayersStore) {
-                currentPocket = (currentPocket + 1) % 14
-            }
-            mancalaModel.pocketStones[currentPocket]++
-            currentPocket = (currentPocket + 1) % 14
-            numStonesToMove--
-        }
-        val lastPocket = if (currentPocket == 0) 13 else currentPocket - 1
-        // check if last stone ends in empty pocket on player's own side. If so, move all stones
-        // in opposite pocket to player's store.
-        val lastPocketOpposite = oppositePocket(lastPocket)
-        if (lastPocket in playersPockets && mancalaModel.pocketStones[lastPocket] == 1
-            && lastPocketOpposite != null) {
-            mancalaModel.pocketStones[playersStore] += mancalaModel.pocketStones[lastPocketOpposite]
-            mancalaModel.pocketStones[lastPocketOpposite] = 0
-        }
-        // check if last stone ends in player's store. If so player gets another turn
-        mancalaModel.player1Turn = if (lastPocket != playersStore) {
-            !mancalaModel.player1Turn
-        } else mancalaModel.player1Turn
-        updateDisplay()
-        if (checkTopEmpty() || checkBottomEmpty()) {
-            for (i in 0..5) {
-                mancalaModel.pocketStones[6] += mancalaModel.pocketStones[i]
-                mancalaModel.pocketStones[i] = 0
-            }
-            for (i in 7..12) {
-                mancalaModel.pocketStones[13] += mancalaModel.pocketStones[i]
-                mancalaModel.pocketStones[i] = 0
-            }
-            mancalaModel.gameOver = true
-            updateDisplay()
-        }
-        if (mancalaModel.gameOver) {
-            showWinnerDialog()
-        }
-    }
-
-    private fun oppositePocket(pocket: Int): Int? {
-        return if (pocket == 6 || pocket == 13) {
-            null
-        } else {
-            12 - pocket
-        }
     }
 }
--- app/src/main/java/com/example/mancalaapplication/MancalaViewModel.kt
+++ app/src/main/java/com/example/mancalaapplication/MancalaViewModel.kt
@@ -3,6 +3,112 @@ package com.example.mancalaapplication
 import androidx.lifecycle.ViewModel
 
 class MancalaViewModel : ViewModel() {
-    //create model which contains data about the board for the UI
-    private val mancala_model = MancalaModel()
+    private var _pocketStones = mutableListOf(4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+    private var _player1Turn = true
+    private var _gameOver = false
+    private val _player1Pockets = listOf(0, 1, 2, 3, 4, 5)
+    private val _player2Pockets = listOf(7, 8, 9, 10, 11, 12)
+
+    val pocketStones: List<Int>
+        get() = _pocketStones.toList()
+    val player1Turn: Boolean
+        get() = _player1Turn
+    val gameOver: Boolean
+        get() = _gameOver
+
+    fun restartGame() {
+        _pocketStones = mutableListOf(4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+        _player1Turn = true
+        _gameOver = false
+    }
+
+    fun checkPlayer1Winner(): Boolean {
+        return _pocketStones[6] > _pocketStones[13]
+    }
+
+    fun pocketWrongSide(pocket: Int): Boolean {
+        return (player1Turn && pocket in _player2Pockets) ||
+            (!player1Turn && pocket in _player1Pockets)
+    }
+
+    fun pocketEmpty(pocket: Int): Boolean {
+        return _pocketStones[pocket] == 0
+    }
+
+    fun moveStones(pocket: Int) {
+        val playersStore = if (_player1Turn) 6 else 13
+        val otherPlayersStore = if (_player1Turn) 13 else 6
+        val playersPockets = if (_player1Turn) {
+            mutableListOf(0, 1, 2, 3, 4, 5)
+        } else {
+            mutableListOf(7, 8, 9, 10, 11, 12)
+        }
+        //val otherPlayersPockets = if (_player1Turn) {
+        //    mutableListOf(7, 8, 9, 10, 11, 12)
+        //} else {
+        //    mutableListOf(0, 1, 2, 3, 4, 5)
+        //}
+        var numStonesToMove: Int = _pocketStones[pocket]
+        _pocketStones[pocket] = 0
+        var currentPocket = pocket + 1
+        // distribute stones into pockets
+        while (numStonesToMove > 0) {
+            if (currentPocket == otherPlayersStore) {
+                currentPocket = (currentPocket + 1) % 14
+            }
+            _pocketStones[currentPocket]++
+            currentPocket = (currentPocket + 1) % 14
+            numStonesToMove--
+        }
+        val lastPocket = if (currentPocket == 0) 13 else currentPocket - 1
+        // check if last stone ends in empty pocket on player's own side. If so, move all stones
+        // in opposite pocket to player's store.
+        val lastPocketOpposite = oppositePocket(lastPocket)
+        if (lastPocket in playersPockets && _pocketStones[lastPocket] == 1
+            && lastPocketOpposite != null) {
+            _pocketStones[playersStore] += _pocketStones[lastPocketOpposite]
+            _pocketStones[lastPocketOpposite] = 0
+        }
+        // check if last stone ends in player's store. If so player gets another turn
+        _player1Turn = if (lastPocket != playersStore) {
+            !_player1Turn
+        } else _player1Turn
+        if (checkTopEmpty() || checkBottomEmpty()) {
+            for (i in 0..5) {
+                _pocketStones[6] += _pocketStones[i]
+                _pocketStones[i] = 0
+            }
+            for (i in 7..12) {
+                _pocketStones[13] += _pocketStones[i]
+                _pocketStones[i] = 0
+            }
+            _gameOver = true
+        }
+    }
+
+    private fun checkBottomEmpty(): Boolean {
+        for (i in 0..5) {
+            if (_pocketStones[i] != 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun checkTopEmpty(): Boolean {
+        for (i in 7..12) {
+            if (_pocketStones[i] != 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun oppositePocket(pocket: Int): Int? {
+        return if (pocket == 6 || pocket == 13) {
+            null
+        } else {
+            12 - pocket
+        }
+    }
 }
